0fde0bc70b2e378b7f253963b4100bc261596b3b
Clinton Blackburn <cblackburn@edx.org>
2016-04-11 20:05:50 -0400

    fixup! Add the real terms of service to our API request page, and improve UI.

--- openedx/core/djangoapps/api_admin/urls.py
+++ openedx/core/djangoapps/api_admin/urls.py
@@ -8,17 +8,17 @@ from openedx.core.djangoapps.api_admin.views import ApiRequestView, ApiRequestSt
 
 urlpatterns = (
     url(
-        r'^status$',
+        r'^status/$',
         api_access_enabled_or_404(login_required(ApiRequestStatusView.as_view())),
         name="api-status"
     ),
     url(
-        r'^terms-of-service$',
+        r'^terms-of-service/$',
         api_access_enabled_or_404(ApiTosView.as_view()),
         name="api-tos"
     ),
     url(
-        r'$',
+        r'^$',
         api_access_enabled_or_404(login_required(ApiRequestView.as_view())),
         name="api-request"
     ),
